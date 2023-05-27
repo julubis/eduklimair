7bcbaa004f17a7acfef7bb73175e4e1fbe0f56ca
rizalabimanyu202 <100014216+rizalabimanyu202@users.noreply.github.com>
2023-05-27 17:53:58 +0700

    Menyelesaikan halaman home dan membuat responsive pada header dan footer agar bisa diterapkan disemua halaman

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import 'regenerator-runtime';
+import '../styles/header.sass';
+import '../styles/responsive.sass';
+import '../styles/mainHome1.sass';
+import '../styles/footer.sass';
+import './navigationDrawer';
+import './tombolLatest';
--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s[ac]ss$/i,
         use: [
           {
             loader: 'style-loader',
