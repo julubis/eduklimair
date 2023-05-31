6208471ff80a63bfa3a9aaf95beda7a53262d9a9
julubis <julubis@github.com>
2023-06-01 06:56:14 +0700

    add routes

---
+++ src/scripts/routes/router.js
@@ -0,0 +1,14 @@
+import Home from '../views/pages/home';
+
+const routes = {
+  '/': Home,
+  '/signin': '',
+  '/signup': '',
+  '/admin': '',
+  '/about': '',
+  '/article/water': '',
+  '/article/climate': '',
+  '/article/:category/:id': '',
+};
+
+export default routes;
